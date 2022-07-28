7dd5f64f9c9a00d68a9a9c3862cffd6058bfafc8
Revan-Darth <nitro.Knight3080@gmail.com>
2022-07-28 23:33:21 +0700

    added adaptive first page, added page service and adaptive

--- js/main.js
+++ js/main.js
@@ -87,9 +87,7 @@ $(document).ready(function(){
 			const popupActive = document.querySelector('.popup.open');
 			if (popupActive) {
 				popupClose(popupActive, false);
-			} else {
-				bodyLock();
-			}
+			} 
 			curentPopup.classList.add('open');
 			curentPopup.addEventListener('click', function (e) {
 				if (!e.target.closest('.popup__content')) {
@@ -101,41 +99,8 @@ $(document).ready(function(){
 	function popupClose(popupActive, doUnlock = true) {
 		if (unlock) {
 			popupActive.classList.remove('open');
-			if (doUnlock){
-				bodyUnLock();
-			}
+	
 		}
-	}
-	function bodyLock() {
-		const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
-		if (lockPadding.length > 0){
-			for (let index = 0; index < lockPadding.length; index++) {
-				const el = lockPadding[index];
-				el.style.paddingRight = lockPaddingValue;
-			}
-		}
-		body.style.paddingRight = lockPaddingValue;
-		body.classList.add('lock');
-		unlock = false;
-		setTimeout(function () {
-			unlock = true;
-		}, timeout);
-	}
-	function bodyUnLock() {
-		setTimeout(function () {
-			if (lockPadding.length > 0) {
-				for (let index = 0; index < lockPadding.length; index++) {
-					const el = lockPadding[index];
-					el.style.paddingRight = '0px';
-				}
-			}
-			body.style.paddingRight = '0px';
-			body.classList.remove('lock');
-		}, timeout);
-		unlock = false;
-		setTimeout(function () {
-			unlock = true;
-		}, timeout);
 	}
 	document.addEventListener('keydown', function (e) {
 		if (e.which === 27) {
@@ -263,3 +228,207 @@ $(document).ready(function(){
 		}, 300 );
 	}
 // Анимация при скролле ==============================================
+// Динамический адаптив ==============================================
+	function DynamicAdapt(type) {
+		this.type = type;
+	}
+
+	DynamicAdapt.prototype.init = function () {
+		const _this = this;
+		// массив объектов
+		this.оbjects = [];
+		this.daClassname = "_dynamic_adapt_";
+		// массив DOM-элементов
+		this.nodes = document.querySelectorAll("[data-da]");
+
+		// наполнение оbjects объктами
+		for (let i = 0; i < this.nodes.length; i++) {
+			const node = this.nodes[i];
+			const data = node.dataset.da.trim();
+			const dataArray = data.split(",");
+			const оbject = {};
+			оbject.element = node;
+			оbject.parent = node.parentNode;
+			оbject.destination = document.querySelector(dataArray[0].trim());
+			оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+			оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+			оbject.index = this.indexInParent(оbject.parent, оbject.element);
+			this.оbjects.push(оbject);
+		}
+
+		this.arraySort(this.оbjects);
+
+		// массив уникальных медиа-запросов
+		this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+			return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+		}, this);
+		this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+			return Array.prototype.indexOf.call(self, item) === index;
+		});
+
+		// навешивание слушателя на медиа-запрос
+		// и вызов обработчика при первом запуске
+		for (let i = 0; i < this.mediaQueries.length; i++) {
+			const media = this.mediaQueries[i];
+			const mediaSplit = String.prototype.split.call(media, ',');
+			const matchMedia = window.matchMedia(mediaSplit[0]);
+			const mediaBreakpoint = mediaSplit[1];
+
+			// массив объектов с подходящим брейкпоинтом
+			const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+				return item.breakpoint === mediaBreakpoint;
+			});
+			matchMedia.addListener(function () {
+				_this.mediaHandler(matchMedia, оbjectsFilter);
+			});
+			this.mediaHandler(matchMedia, оbjectsFilter);
+		}
+	};
+
+	DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+		if (matchMedia.matches) {
+			for (let i = 0; i < оbjects.length; i++) {
+				const оbject = оbjects[i];
+				оbject.index = this.indexInParent(оbject.parent, оbject.element);
+				this.moveTo(оbject.place, оbject.element, оbject.destination);
+			}
+		} else {
+			for (let i = 0; i < оbjects.length; i++) {
+				const оbject = оbjects[i];
+				if (оbject.element.classList.contains(this.daClassname)) {
+					this.moveBack(оbject.parent, оbject.element, оbject.index);
+				}
+			}
+		}
+	};
+
+	// Функция перемещения
+	DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+		element.classList.add(this.daClassname);
+		if (place === 'last' || place >= destination.children.length) {
+			destination.insertAdjacentElement('beforeend', element);
+			return;
+		}
+		if (place === 'first') {
+			destination.insertAdjacentElement('afterbegin', element);
+			return;
+		}
+		destination.children[place].insertAdjacentElement('beforebegin', element);
+	}
+
+	// Функция возврата
+	DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+		element.classList.remove(this.daClassname);
+		if (parent.children[index] !== undefined) {
+			parent.children[index].insertAdjacentElement('beforebegin', element);
+		} else {
+			parent.insertAdjacentElement('beforeend', element);
+		}
+	}
+
+	// Функция получения индекса внутри родителя
+	DynamicAdapt.prototype.indexInParent = function (parent, element) {
+		const array = Array.prototype.slice.call(parent.children);
+		return Array.prototype.indexOf.call(array, element);
+	};
+
+	// Функция сортировки массива по breakpoint и place 
+	// по возрастанию для this.type = min
+	// по убыванию для this.type = max
+	DynamicAdapt.prototype.arraySort = function (arr) {
+		if (this.type === "min") {
+			Array.prototype.sort.call(arr, function (a, b) {
+				if (a.breakpoint === b.breakpoint) {
+					if (a.place === b.place) {
+						return 0;
+					}
+
+					if (a.place === "first" || b.place === "last") {
+						return -1;
+					}
+
+					if (a.place === "last" || b.place === "first") {
+						return 1;
+					}
+
+					return a.place - b.place;
+				}
+
+				return a.breakpoint - b.breakpoint;
+			});
+		} else {
+			Array.prototype.sort.call(arr, function (a, b) {
+				if (a.breakpoint === b.breakpoint) {
+					if (a.place === b.place) {
+						return 0;
+					}
+
+					if (a.place === "first" || b.place === "last") {
+						return 1;
+					}
+
+					if (a.place === "last" || b.place === "first") {
+						return -1;
+					}
+
+					return b.place - a.place;
+				}
+
+				return b.breakpoint - a.breakpoint;
+			});
+			return;
+		}
+	};
+
+	const da = new DynamicAdapt("max");
+	da.init();
+// Динамический адаптив ==============================================
+// Меню бургер =======================================================
+	const menuBurger = document.querySelector('.menu__burger');
+	const menuBody = document.querySelector('.header__menu');
+	if(menuBurger) {
+		menuBurger.addEventListener('click', function (e) {
+			document.body.classList.toggle('lock');
+			menuBurger.classList.toggle('active');
+			menuBody.classList.toggle('active');
+		});
+	}
+// Меню бургер =======================================================
+
+new Swiper('.protect__inner', {
+	slidesPerView: 1,
+  	spaceBetween: 75,
+  	freeMode: true,
+  	breakpoints: {
+  		768: {
+  			slidesPerView: 2,
+  			spaceBetween: 50,
+  		},
+  		992: {
+  			slidesPerView: 2.5,
+  			spaceBetween: 25,
+  		},
+  		1366: {
+  			slidesPerView: 3,
+  			spaceBetween: 15,
+  		},
+  		1500: {
+  			slidesPerView: 3.7,
+  			spaceBetween: 5,
+  		},
+  	}
+});
+
+// Читать далее =======================================================
+	const readMoreCont = document.querySelector('.protect__text');
+	if (readMoreCont) {
+		readMoreCont.addEventListener('click', event => {
+			const current = event.target;
+			const isReadMoreBtn = current.className.includes('protect__read-more--btn');
+			if (!isReadMoreBtn) return;
+			const currentText = event.target.parentNode.querySelector('.protect__read-more');
+			currentText.classList.toggle('protect__read-more--show');
+			current.textContent = current.textContent.includes('Читать далее') ? "Свернуть" : "Читать далее...";
+		});
+	}
+// Читать далее =======================================================
